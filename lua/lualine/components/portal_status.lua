a66961115cc3358a93e3d4b71fc7c4221d2e6d2d
Calvin Bochulak <cbochula@cisco.com>
2022-11-02 19:37:42 -0600

    feat(unify): remove lualine integration

--- lua/lualine/components/portal_status.lua
+++
@@ -1,9 +0,0 @@
-return function()
-    local highlight = require("portal.highlight")
-    local tag = require("portal.tag")
-    if tag.exists() then
-        return "%#" .. highlight.groups.leap_tag_active .. "#" .. "[M]" .. "%*"
-    else
-        return "%#" .. highlight.groups.leap_tag_inactive .. "#" .. "[U]" .. "%*"
-    end
-end
