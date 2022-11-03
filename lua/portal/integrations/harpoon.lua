213ec779d906bdfa64f94f521534afa7ddd856ec
Calvin Bochulak <cbochula@cisco.com>
2022-11-03 11:57:51 -0600

    fix(integrations): use the correct buffer for the "harpoon" query item

--- lua/portal/integrations/harpoon.lua
+++ lua/portal/integrations/harpoon.lua
@@ -6,7 +6,7 @@ local function is_marked(jump)
     local query = require("portal.query")
     local util = require("harpoon.util")
     local mark = require("harpoon.mark")
-    local buffer_name = util.normalize_path(vim.api.nvim_buf_get_name(0))
+    local buffer_name = util.normalize_path(vim.api.nvim_buf_get_name(jump.buffer))
     return query.valid.predicate(jump) and query.different.predicate(jump) and mark.mark_exists(buffer_name)
 end
 
