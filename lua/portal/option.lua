23689b74817c98578b4078f282f678fd71874af6
Calvin Bochulak <calvin.bochulak@gmail.com>
2023-02-27 16:00:31 -0700

    refactor: add Option:none and Option:some methods

--- lua/portal/option.lua
+++ lua/portal/option.lua
@@ -1,10 +1,28 @@
+---@class Portal.Option
+---@field value any
 local Option = {}
 
+---@param value any
+---@return Portal.Option
 function Option:new(value)
     local option = { value = value }
+
     setmetatable(option, self)
     self.__index = self
+
     return option
+end
+
+local NONE = Option:new()
+
+-- luacheck: ignore
+function Option:none()
+    return NONE
+end
+
+-- luacheck: ignore
+function Option:some(value)
+    return Option:new(value)
 end
 
 function Option:is_some()
@@ -15,13 +33,16 @@ function Option:is_none()
     return not self:is_some()
 end
 
+---@return any
 function Option:unwrap()
     if self:is_none() then
-        error("Cannot unwrap None-type")
+        error("Cannot unwrap None-type.")
     end
     return self.value
 end
 
+---@param default_value any
+---@return any
 function Option:unwrap_or(default_value)
     return self:is_some() and self.value or default_value
 end
