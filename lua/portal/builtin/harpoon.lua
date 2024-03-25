84c49aad1898fd326bd41a6ce7b766759bf65725
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-25 09:33:52 -0600

    fix(builtin): move harpoon to new extension format

--- lua/portal/builtin/harpoon.lua
+++ lua/portal/builtin/harpoon.lua
@@ -1,78 +1,46 @@
----@type Portal.QueryGenerator
-local function generator(opts, settings)
-    local Content = require("portal.content")
-    local Iterator = require("portal.iterator")
-    local Search = require("portal.search")
+return require("portal.extension").register({
+    name = "harpoon",
 
-    local ok, _ = pcall(require, "harpoon")
-    if not ok then
-        return require("portal.log").error("Unable to load 'harpoon'. Please ensure that harpoon is installed.")
-    end
-
-    local marks = require("harpoon").get_mark_config().marks
-
-    opts = vim.tbl_extend("force", {
-        direction = "forward",
-        max_results = #settings.labels,
-    }, opts or {})
-
-    if settings.max_results then
-        opts.max_results = math.min(opts.max_results, settings.max_results)
-    end
-
-    -- stylua: ignore
-    local iter = Iterator:new(marks)
-        :take(settings.lookback)
-
-    if opts.start then
-        iter = iter:start_at(opts.start)
-    end
-    if opts.direction == Search.direction.backward then
-        iter = iter:reverse()
-    end
-
-    iter = iter:map(function(v, i)
-        local buffer
-        if vim.fn.bufexists(v.filename) ~= 0 then
-            buffer = vim.fn.bufnr(v.filename)
-        else
-            buffer = vim.fn.bufadd(v.filename)
+    ---@return Portal.Result[] results, Portal.QueryOptions defaults
+    generate = function()
+        local ok, _ = pcall(require, "harpoon")
+        if not ok then
+            error("Portal: the 'harpoon' builtin requires the plugin 'ThePrimagen/harpoon'")
         end
 
-        if buffer == vim.fn.bufnr() then
-            return nil
-        end
+        local marks = require("harpoon"):list()
 
-        return Content:new({
-            type = "harpoon",
-            buffer = buffer,
-            cursor = { row = v.row, col = v.col },
-            callback = function(content)
-                require("harpoon.ui").nav_file(content.extra.index)
+        ---@type Portal.QueryOptions
+        local defaults = {
+            filter = function(content)
+                return vim.api.nvim_buf_get_name(0) ~= content.path
             end,
+        }
+
+        return marks, defaults
+    end,
+
+    ---@param index integer
+    ---@param extended_result Portal.ExtendedResult
+    ---@return Portal.Content
+    transform = function(index, extended_result)
+        local mark = extended_result.result
+
+        ---@type Portal.Content
+        local content = {
+            type = "harpoon",
+            path = mark.value,
+            cursor = mark.cursor or { mark.context.row or 1, mark.context.col or 0 },
             extra = {
-                index = i,
+                index = index,
             },
-        })
-    end)
+        }
 
-    iter = iter:filter(function(v)
-        return vim.api.nvim_buf_is_valid(v.buffer)
-    end)
-    if settings.filter then
-        iter = iter:filter(settings.filter)
-    end
-    if opts.filter then
-        iter = iter:filter(opts.filter)
-    end
-    if not opts.slots then
-        iter = iter:take(opts.max_results)
-    end
+        return content
+    end,
 
-    return {
-        source = iter,
-        slots = opts.slots,
-    }
-end
-
-return generator
+    ---@param content Portal.Content
+    select = function(content)
+        require("harpoon"):list():select(content.extra.index)
+    end,
+})
