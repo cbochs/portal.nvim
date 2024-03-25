e39d3ba0a4b6534e2ec3c3534dc8d600fa1d50bf
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-24 20:49:35 -0600

    refactor: update builtins to support new extension system

--- lua/portal/builtin/quickfix.lua
+++ lua/portal/builtin/quickfix.lua
@@ -1,60 +1,35 @@
----@type Portal.QueryGenerator
-local function generator(opts, settings)
-    local Content = require("portal.content")
-    local Iterator = require("portal.iterator")
-    local Search = require("portal.search")
+return require("portal.extension").register({
+    name = "quickfix",
 
-    local quickfix = vim.fn.getqflist()
+    ---@return Portal.Result[] results, Portal.QueryOptions? defaults
+    generate = function()
+        local quickfix = vim.fn.getqflist()
 
-    opts = vim.tbl_extend("force", {
-        direction = "forward",
-        max_results = #settings.labels,
-    }, opts or {})
-
-    if settings.max_results then
-        opts.max_results = math.min(opts.max_results, settings.max_results)
-    end
-
-    -- stylua: ignore
-    local iter = Iterator:new(quickfix)
-        :take(settings.lookback)
-
-    if opts.start then
-        iter = iter:start_at(opts.start)
-    end
-    if opts.direction == Search.direction.backward then
-        iter = iter:reverse()
-    end
-
-    iter = iter:map(function(v, _)
-        return Content:new({
-            type = "quickfix",
-            buffer = v.bufnr,
-            cursor = { row = v.lnum, col = v.col },
-            callback = function(content)
-                vim.api.nvim_win_set_buf(0, content.buffer)
-                vim.api.nvim_win_set_cursor(0, { content.cursor.row, content.cursor.col })
+        ---@type Portal.QueryOptions
+        local defaults = {
+            filter = function(content)
+                return vim.api.nvim_buf_is_valid(content.buffer)
             end,
-        })
-    end)
+        }
 
-    iter = iter:filter(function(v)
-        return vim.api.nvim_buf_is_valid(v.buffer)
-    end)
-    if settings.filter then
-        iter = iter:filter(settings.filter)
-    end
-    if opts.filter then
-        iter = iter:filter(opts.filter)
-    end
-    if not opts.slots then
-        iter = iter:take(opts.max_results)
-    end
+        return quickfix, defaults
+    end,
 
-    return {
-        source = iter,
-        slots = opts.slots,
-    }
-end
+    ---@param _ integer
+    ---@param extended_result Portal.ExtendedResult
+    ---@return Portal.Content
+    transform = function(_, extended_result)
+        local result = extended_result.result
 
-return generator
+        return {
+            type = "quickfix",
+            buffer = result.bufnr,
+            cursor = { result.lnum, result.col },
+        }
+    end,
+
+    select = function(content)
+        vim.api.nvim_win_set_buf(0, content.buffer)
+        vim.api.nvim_win_set_cursor(0, { content.cursor[1], content.cursor[2] })
+    end,
+})
