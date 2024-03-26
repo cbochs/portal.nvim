81a673ee9ca6c8974bbe2788da276a9e959d4d4a
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-26 10:02:26 -0600

    fix(test): update query and search specs

--- tests/spec/portal/query_spec.lua
+++ tests/spec/portal/query_spec.lua
@@ -2,32 +2,23 @@ local Iter = require("portal.iterator")
 local Query = require("portal.query")
 
 local function generator()
-    return Iter.iter({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+    return Iter.iter({ 0, 1, 2, 3, 4 })
 end
 
 describe("search", function()
     describe("#search", function()
         it("performs a search", function()
             local query = Query.new(generator)
-            assert.are.same({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, query:search())
+            assert.are.same({ 0, 1, 2, 3, 4 }, query:search():totable())
         end)
 
-        it("searches for a set number of slots", function()
-            local query = Query.new(generator)
-            assert.are.same({ 0, 1, 2 }, query:search(3))
-        end)
-
-        it("searches for a set specific set of slots", function()
-            local query = Query.new(generator)
-
-            -- stylua: ignore
-            local slots = {
-                function(v) return v % 2 == 0 end,
-                function(v) return v % 2 == 0 end,
-                function(v) return v % 2 == 0 end,
-            }
-
-            assert.are.same({ 0, 2, 4 }, query:search(slots))
+        it("performs a filtered search", function()
+            local query = Query.new(generator):prepare({
+                filter = function(v)
+                    return v % 2 == 0
+                end,
+            })
+            assert.are.same({ 0, 2, 4 }, query:search():totable())
         end)
     end)
 end)
