c78b6b42fda1c84f2b135f51aee7ff2896f2a526
Calvin Bochulak <calvin.bochulak@gmail.com>
2024-03-26 10:02:26 -0600

    fix(test): update query and search specs

--- tests/spec/portal/search_spec.lua
+++ tests/spec/portal/search_spec.lua
@@ -8,17 +8,27 @@ end
 
 describe("portal", function()
     describe("#search", function()
-        it("returns results for a single query", function()
-            local query = Query.new(generator)
-            assert.are.same({ 1, 2, 3 }, Search.search(query))
-        end)
-
-        it("returns results for multiple queries", function()
+        it("returns results for a set of queries", function()
             local queries = {
                 Query.new(generator),
                 Query.new(generator),
             }
             assert.are.same({ 1, 2, 3, 1, 2, 3 }, Search.search(queries))
+        end)
+
+        it("returns a maximum number of results", function()
+            local queries = { Query.new(generator) }
+            assert.are.same({ 1 }, Search.search(queries, 1))
+        end)
+
+        it("returns results for a set of slots", function()
+            local queries = { Query.new(generator) }
+
+            -- stylua: ignore
+            assert.are.same({ 3, 1 }, Search.search(queries, {
+                function(v) return v == 3 end,
+                function(v) return v == 1 end,
+            }))
         end)
     end)
 end)
